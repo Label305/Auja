fa4687414aaba0079211da5e514fb26b1e7b0fe5
Joris Blaak <joris@label305.com>
2014-08-29 13:50:20 +0200

    Initial styling and propagating of main color

--- src/Components/Scaffolding/header.react.js
+++ src/Components/Scaffolding/header.react.js
@@ -5,9 +5,13 @@ define([], function() {
 
     return React.createClass({
         render: function() {
+            var headerStyle = {
+                color: this.props.auja.colors.main
+            };
+            
             return (
                 <header>
-                    <h1>{this.props.auja.title}</h1>
+                    <h1 style={headerStyle}>{this.props.auja.title}</h1>
                 </header>
                 );
         }
--- src/Components/Scaffolding/menu.react.js
+++ src/Components/Scaffolding/menu.react.js
@@ -10,8 +10,16 @@ define([], function() {
             flux.actions.click(this.props.item.url);
         },
         render: function() {
+            
+            //Pass the main color as background
+            var style = {
+                backgroundColor: this.props.auja.colors.main
+            };
+            
             return (
-                <li title={this.props.item.title} onClick={this.handleClick}>{this.props.item.title}</li>
+                <li style={style} title={this.props.item.title} onClick={this.handleClick}>
+                    <p>{this.props.item.title}</p>
+                </li>
                 );
         }
     });
@@ -20,7 +28,7 @@ define([], function() {
         render: function() {
             var menu = this.props.auja.menu.map(function(item) {
                 return (
-                    <Item item={item} />
+                    <Item auja={this.props.auja} item={item} />
                     );
             }.bind(this));
             
