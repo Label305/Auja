90e56b7ac3fa00853d627b1c49744d612cee822f
Joris Blaak <joris@label305.com>
2014-08-31 09:57:06 +0200

    Added username and buttons to header

--- src/Components/Scaffolding/header.react.js
+++ src/Components/Scaffolding/header.react.js
@@ -1,20 +1,48 @@
 /**
  * @jsx React.DOM
  */
-define([], function() {
+define([], function () {
 
     return React.createClass({
-        render: function() {
+        render: function () {
             var headerStyle = {
                 color: this.props.auja.colors.main
             };
-            
+
+            //Name of the user
+            var user = '';
+            if (this.props.auja.user) {
+                var userStyle = {
+                    color: this.props.auja.colors.main
+                };
+                user = (
+                    <div style={userStyle} id="user">{this.props.auja.user.name}</div>
+                    );
+            }
+
+            //Buttons, e.g. logout
+            var buttons = '';
+            if (this.props.auja.buttons) {
+                var buttonStyle = {
+                    backgroundColor: this.props.auja.colors.main
+                };
+
+                buttons = this.props.auja.buttons.map(function (button) {
+                    return (
+                        <a style={buttonStyle} href={button.url}>{button.title}</a>
+                        );
+                });
+            }
             return (
                 <header>
                     <h1 style={headerStyle}>{this.props.auja.title}</h1>
+                    <div id="buttons">{buttons}</div>
+                    {user}
                 </header>
                 );
+
         }
     });
 
 });
+                        
