17c096c2999fd4dbd9f917197a81d90bd307f7c8
Joris Blaak <joris@label305.com>
2014-08-31 11:07:06 +0200

    Basic styling of menu

--- src/Components/Panels/Menu/link.react.js
+++ src/Components/Panels/Menu/link.react.js
@@ -13,9 +13,21 @@ define([], function() {
         handleClick: function() {
             flux.actions.click(this.props.item.link.href, this.props.panel);
         },
-        render: function() {            
+        render: function() {
+
+            var className = 'menu-item-link auja-border-secondary ';
+            
+            //Create the icon class
+            var icon = "fallback";
+            if(this.props.item.icon) {
+                icon = this.props.item.icon;    
+            }
+            className += "icon-" + icon;
+            
             return (
-                <li onClick={this.handleClick}>{this.props.item.link.name}</li>
+                <li className={className} onClick={this.handleClick}>
+                    <p>{this.props.item.link.name}</p>
+                </li>
                 );
         }
     });
--- src/scaffolding.react.js
+++ src/scaffolding.react.js
@@ -5,46 +5,54 @@ define([
     'build/Stores/auja',
     'build/Components/Scaffolding/header.react',
     'build/Components/Scaffolding/body.react'
-], function(Store, Header, Body) {
-    
+], function (Store, Header, Body) {
+
     var Style = React.createClass({
-        entry: function(name, property_name, value) {
+        /**
+         * Combine items into a neatly formatted string
+         * @param name will be prefixed with ".auja-"
+         * @param property_name the CSS property to be styled
+         * @param value the value it should take
+         * @returns {string}
+         */
+        entry: function (name, property_name, value) {
             var result = "";
             result += ".auja-" + name + " {\n";
-            result += "\t" + property_name + ": " + value + ";\n";
+            result += "\t" + property_name + ": " + value + "!important;\n";
             result += "}\n\n";
             return result;
         },
-        parse: function(colors) {
+        parse: function (colors) {
             var result = "\n";
-            for(var name in colors) {
-                result += this.entry('bg-' + name, 'background-color', colors[name]);                
-                result += this.entry('color-' + name, 'color', colors[name]);        
+            for (var name in colors) {
+                result += this.entry('bg-' + name, 'background-color', colors[name]);
+                result += this.entry('color-' + name, 'color', colors[name]);
+                result += this.entry('border-' + name, 'border-color', colors[name]);
             }
             return result;
         },
-        render: function() {
+        render: function () {
             var style = this.parse(this.props.auja.colors);
-            
+
             return (
                 <style>
                     {style}
                 </style>
                 );
-        } 
+        }
     });
-    
+
     return React.createClass({
         mixins: [Fluxxor.FluxMixin(React), Fluxxor.StoreWatchMixin('AujaStore')],
-        getStateFromFlux: function() {
-            return flux.store('AujaStore').getState();                
+        getStateFromFlux: function () {
+            return flux.store('AujaStore').getState();
         },
-        componentWillMount: function() {
+        componentWillMount: function () {
             flux.actions.initialize();
         },
-        render: function() {
-            document.title = this.state.title;    
-            
+        render: function () {
+            document.title = this.state.title;
+
             return (
                 <div id="auja">
                     <Header auja={this.state} />
@@ -54,5 +62,5 @@ define([
                 );
         }
     });
-    
+
 });
