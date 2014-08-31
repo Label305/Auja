cab720241530caaf6e8aa31f47c238c7abbdc0e0
Joris Blaak <joris@label305.com>
2014-08-31 12:13:07 +0200

    Loading off crossroads

--- src/Utils/request.js
+++ src/Utils/request.js
@@ -3,75 +3,88 @@
  * the goal is to add routing, options for factories to generate responses etc.etc.
  * @param string url
  */
-window.Request = function () {
+define(['signals', 'crossroads'], function (signals, crossroads) {
+    
+    //Register as a global
+    window.crossroads = crossroads;
 
     /**
-     * The url to do the request o
-     * @type String
+     * Setup listening to the AujaStore to update routes in crossroads
      */
-    this.url = arguments[0] ? arguments[0] : window.location;
+    flux.store('AujaStore').on('change', function() {
+        crossroads.removeAllRoutes();
+    });
+    
+    window.Request = function () {
 
-    /**
-     * Default request
-     * @type Object
-     */
-    this.settings = {
-        'data': {},
-        'dataType': 'json',
-        'cache': false,
-        'isFile': false
-    };
+        /**
+         * The url to do the request o
+         * @type String
+         */
+        this.url = arguments[0] ? arguments[0] : window.location;
 
-    /**
-     * Setter for data to requests
-     * @param data
-     */
-    this.setData = function (data) {
-        this.settings.data = data;
-    };
+        /**
+         * Default request
+         * @type Object
+         */
+        this.settings = {
+            'data': {},
+            'dataType': 'json',
+            'cache': false,
+            'isFile': false
+        };
 
-    /**
-     * Get request
-     * @return Deferred
-     * @param callback
-     */
-    this.get = function (callback) {
-        this.settings.type = 'GET';
-        return this._doAjax().done(function (response) {
-            if (typeof callback == 'function') callback(response);
-        });
-    };
+        /**
+         * Setter for data to requests
+         * @param data
+         */
+        this.setData = function (data) {
+            this.settings.data = data;
+        };
 
-    /**
-     * Post request
-     * @return Deferred
-     * @param callback
-     */
-    this.post = function (callback) {
-        this.settings.type = 'POST';
-        return this._doAjax().done(function (response) {
-            if (typeof callback == 'function') callback(response);
-        });
-    };
+        /**
+         * Get request
+         * @return Deferred
+         * @param callback
+         */
+        this.get = function (callback) {
+            this.settings.type = 'GET';
+            return this._doAjax().done(function (response) {
+                if (typeof callback == 'function') callback(response);
+            });
+        };
 
-    /**
-     * Put request
-     * @return Deferred
-     * @param callback
-     */
-    this.put = function (callback) {
-        this.settings.data._method = 'PUT';
-        return this.post(callback);
-    };
+        /**
+         * Post request
+         * @return Deferred
+         * @param callback
+         */
+        this.post = function (callback) {
+            this.settings.type = 'POST';
+            return this._doAjax().done(function (response) {
+                if (typeof callback == 'function') callback(response);
+            });
+        };
 
-    /**
-     * Actual executor
-     * @return Deferred
-     */
-    this._doAjax = function () {
-        if (!this.settings.isFile && this.url.substr(-5) != '.json') {
-            this.url += '.json';
+        /**
+         * Put request
+         * @return Deferred
+         * @param callback
+         */
+        this.put = function (callback) {
+            this.settings.data._method = 'PUT';
+            return this.post(callback);
+        };
+
+        /**
+         * Actual executor
+         * @return Deferred
+         */
+        this._doAjax = function () {
+            if (!this.settings.isFile && this.url.substr(-5) != '.json') {
+                this.url += '.json';
+            }
+            return $.ajax(this.url, this.settings);
         }
-        return $.ajax(this.url, this.settings);
-    }
-};
+    };
+});
--- src/auja.react.js
+++ src/auja.react.js
@@ -4,7 +4,7 @@
 
 
 //Debugging
-if(typeof __debug__ != 'undefined') {
+if (typeof __debug__ != 'undefined') {
     var __debug = false;
 }
 
@@ -15,11 +15,11 @@ require.config({
      * Use document base url
      */
     baseUrl: '',
-    
+
     /**
      * When debug is true add a bust to prevent caching
      */
-    urlArgs: __debug__ ? '_=' +  (new Date()).getTime() : '',
+    urlArgs: __debug__ ? '_=' + (new Date()).getTime() : '',
 
     /**
      * Location of dependencies
@@ -28,6 +28,8 @@ require.config({
         react: 'bower_components/react/react',
         jquery: 'bower_components/jquery/dist/jquery.min',
         fluxxor: 'bower_components/fluxxor/build/fluxxor',
+        signals: 'bower_components/js-signals/dist/signals',
+        crossroads: 'bower_components/crossroads.js/dist/crossroads',
         request: 'build/Utils/request',
         response_handler: 'build/Utils/response_handler',
         stores: 'build/Stores/flux'
@@ -36,31 +38,35 @@ require.config({
 
 /**
  * Load initial dependencies:
- * 
+ *
  * react - The ReactJS library from Facebook
  * jquery - Used as a toolkit to make life easier
  * fluxxor - To be able to implement the Flux architecture
  * request - Every request starts here, it'll take care of factories and routing
  * response_handler - Every response will end here after which it will dispatch
  */
-require(['react', 'jquery', 'fluxxor', 'request', 'response_handler'], function(react) {
-    
+require(['react', 'jquery', 'fluxxor', 'crossroads'], function (react) {
+
     //Register as a global since we'll be using it everywhere
     window.React = react;
-    
+
     //Register as global
     window.Fluxxor = require('fluxxor');
-        
-    //Require the main building block and initialize it
-    require(['build/scaffolding.react', 'build/Stores/flux'], function(Scaffolding, flux) {
+
+    //Setup data layer
+    require(['build/Stores/flux'], function (flux) {
         
         //Register as global
         window.flux = flux;
         
-        //Render the main structure
-        React.renderComponent(<Scaffolding flux={flux} />, document.body);
+        //Setup building block and its utilities
+        require(['build/scaffolding.react', 'request', 'response_handler'], function(Scaffolding) {
+
+            //Render the main structure
+            React.renderComponent(<Scaffolding flux={flux} />, document.body);
+        });
     });
-    
+
 });
 
     
