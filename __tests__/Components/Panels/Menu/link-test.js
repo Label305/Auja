578df374c0f29ee98ba0155514efb94d18558c57
Joris Blaak <joris@label305.com>
2014-08-31 19:55:01 +0200

    Setup testing using Jest

---
+++ __tests__/Components/Panels/Menu/link-test.js
@@ -0,0 +1,28 @@
+/** 
+ * @jsx React.DOM 
+ */
+//jest.dontMock('../../../../src/Components/Panels/Menu/link.react.js');
+describe('MenuItemLInk', function() {
+    it('displays the link name', function() {
+        //TODO debug the requiring of react/addons breaking our tests!
+        
+//        var React = require('react/addons');
+//        var MenuItemLink = require('../../../../src/Components/Panels/Menu/link.react.js');
+//        var TestUtils = React.addons.TestUtils;
+//                
+//        var item = {
+//            type: 'link',
+//            link: {
+//                name: 'Nameoflink'
+//            }
+//        };
+//        
+//        var link = TestUtils.renderIntoDocument(
+//            <MenuItemLink item={item} />
+//            );
+//        
+//        var li = TestUtils.findRenderedDOMComponentWithTag(link, 'li');
+//        expect(li.getDOMNode().textContent).toContain('Nameoflink');
+        
+    });
+});
