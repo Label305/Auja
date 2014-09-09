5ef945d6bea1fb8fd60dff06f70e462cfed59df3
Alexander van Brakel <alexander@label305.com>
2014-09-09 14:59:27 +0200

    Added onChange handler to number input

--- src/Components/Panels/Page/Form/number.react.js
+++ src/Components/Panels/Page/Form/number.react.js
@@ -7,23 +7,31 @@
  *
  * @jsx React.DOM
  */
-define(['build/Components/Panels/Page/Form/label.react'], function(Label) {
+ define(['build/Components/Panels/Page/Form/label.react'], function(Label) {
     return React.createClass({
+        getInitialState: function() {
+            return {value: this.props.item.number.value};
+        },
+        handleChange: function(event) {
+            this.setState({value: event.target.value});
+        },
         render: function() {
             var attributes = Object.clone(this.props.item.number);
-            
+
             //Remove otherwise engaged attributes
             delete attributes.label;
             
             Object.merge(attributes, {
                 id: this.props.itemId,
-                type: 'number'
-            });
+                type: 'number',
+                onChange: this.handleChange,
+                value: this.state.value
+            });            
             
             return (
                 <div>
-                    <Label item={this.props.item} name={this.props.item.number.label} />
-                    {React.DOM.input(attributes)}
+                <Label item={this.props.item} name={this.props.item.number.label} />
+                {React.DOM.input(attributes)}
                 </div>
                 );
         }
