284d16e1ad4414bf40f51e7aafe051de60b3e1a5
Joris Blaak <joris@label305.com>
2014-10-06 14:02:39 +0200

    Added origin panel as property for a menu

--- src/Components/Panels/Menu/resource.react.js
+++ src/Components/Panels/Menu/resource.react.js
@@ -6,23 +6,23 @@
  * @jsx React.DOM
  */
 
-define(['build/Components/Panels/Menu/Mixins/paging.mixin', 'build/Objects/menu'], function(Paging, Menu) {
-    
+define(['build/Components/Panels/Menu/Mixins/paging.mixin', 'build/Objects/menu'], function (Paging, Menu) {
+
     return React.createClass({
         mixins: [Paging],
-        render: function() {     
+        render: function () {
             var MenuPanel = require('build/Components/Panels/menu.react');
-                        
+
             //Transfer different props to mock Menu
             var panel = new Menu();
             panel.setOrigin(this.props.panel.getOrigin());
             panel.setItems(this.props.item.getItems());
-            
+
             return (
                 <li className="menu-item-resource">
-                    <MenuPanel panel={panel} />
+                    <MenuPanel panel={panel} originPanel={this.props.panel} />
                 </li>
-                );
+            );
         }
     });
 
