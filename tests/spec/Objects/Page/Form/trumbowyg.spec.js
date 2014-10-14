906ee979a5ab6b5a95bfc2603c40f3ea934e1fff
Joris Blaak <joris@label305.com>
2014-10-14 09:49:18 +0200

    Added spec tests for form items

---
+++ tests/spec/Objects/Page/Form/trumbowyg.spec.js
@@ -0,0 +1,59 @@
+define(['build/Objects/Page/Form/trumbowyg'], function (Item) {
+    describe('Trumbowyg RTE form item', function () {
+
+        beforeEach(function () {
+            this.addMatchers(require('jasmine_matchers'));
+        });
+        
+        it('should return corresponding attributes', function () {
+            var item = new Item({
+                order: 10,
+                name: 'thename',
+                label: 'The name',
+                value: 'James',
+                required: true
+            });
+
+            expect(item.getAttributes()).toHaveKeys([
+                'type',
+                'value',
+                'name'
+            ]);
+
+        });
+        
+        it('should implement a form item', function () {
+
+            var item = new Item({
+                order: 10,
+                name: 'thename',
+                label: 'The name',
+                value: 'James',
+                required: true
+            });
+
+            //Text spec of transferring initial data
+            expect(item.getType()).toBe('trumbowyg');
+            expect(item.getOrder()).toBe(10);
+            expect(item.getName()).toBe('thename');
+            expect(item.getLabel()).toBe('The name');
+            expect(item.getValue()).toBe('James');
+            expect(item.isRequired()).toBe(true);
+
+            //Test spec of setters
+            item.setOrder(2);
+            item.setName('somename');
+            item.setLabel('Your name');
+            item.setValue('Charles');
+            item.setRequired(false);
+            
+            expect(item.getOrder()).toBe(2);
+            expect(item.getName()).toBe('somename');
+            expect(item.getLabel()).toBe('Your name');
+            expect(item.getValue()).toBe('Charles');
+            expect(item.isRequired()).toBe(false);
+            
+        });
+
+    });
+});
