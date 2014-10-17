a18dafe89357783ad50a3a8be78c834950250482
Joris Blaak <joris@label305.com>
2014-10-17 12:50:56 +0200

    Added React compiling using Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        react: {
+            dynamic_mappings: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: './src',
+                        src: ['**/*.js'],
+                        dest: './build'
+                    }
+                ]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-react');
+};
