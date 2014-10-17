62bc70da1e58f44508ec82980cf9bf25367cacaf
Joris Blaak <joris@label305.com>
2014-10-17 13:47:26 +0200

    Added release compiling after success build

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,36 @@ module.exports = function(grunt) {
                     }
                 ]
             }
+        },
+        requirejs: {
+            compile: {
+                options: {    //Base from which the project is looked at
+                    baseUrl: '.',
+
+                    //Config file also used by production
+                    mainConfigFile: 'build/auja.react.js',
+
+                    //Licence references can be found in READMEs
+                    preserveLicenseComments: false,
+
+                    //Let r.js search through all files
+                    findNestedDependencies: true,
+
+                    //Include
+                    include: [
+                        'bower_components/requirejs/require.js',
+                        'build/auja.react.js'
+                    ],
+                    
+                    //Output file
+                    out: "dist/auja.min.js"
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-react');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
     
     grunt.registerTask('test', ['react']);
+    grunt.registerTask('release', ['react', 'requirejs']);
 };
