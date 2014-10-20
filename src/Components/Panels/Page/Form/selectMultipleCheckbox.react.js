ca0b72d630ced555f1e42f235a61884983174503
Alexander van Brakel <alexander@label305.com>
2014-10-20 11:16:14 +0200

    Made checkboxes work

--- src/Components/Panels/Page/Form/selectMultipleCheckbox.react.js
+++ src/Components/Panels/Page/Form/selectMultipleCheckbox.react.js
@@ -13,27 +13,34 @@
             return {options: this.props.item.getOptions(),
                 checked: this.props.item.isChecked()};
         },
-        handleChange: function(event) {
-            this.setState({checked: event.target.checked});
-        },
+        handleChange: function(label) {
+            var state = this.state.options.map(function(option) {
+            return {
+                label: option.label,
+                checked: (option.label === label ? !option.checked : option.checked)
+            };
+        });
+
+        this.setState({ options: state });
+
+    },
         render: function () {           
-
-            var options = this.props.item.getOptions().map(function(option) {
+            var options = this.props.item.getOptions();
+            var selected = this.state.options.map(function(option) {
             return (
-
                 <div>
-                    <input type="checkbox" checked={option.checked} onChange={this.handleChange}/>
+                    <input type="checkbox" checked={option.checked} onChange={this.handleChange.bind(this, option.label)}/>
                     {option.label}
                     <br />
                 </div>
                 );
-            });
+            }.bind(this));
             
                  
             return (
                 <div>
                 <Label item={this.props.item} name={this.props.item.getLabel()} />
-                {options}
+                {selected}
                 </div>
             );
         }
