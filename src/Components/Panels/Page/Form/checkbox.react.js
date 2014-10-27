f2cfd64601999444b1df6ae1fbc9475cdfbcf0d1
Alexander van Brakel <alexander@label305.com>
2014-10-27 17:03:33 +0100

    Initial commit

--- src/Components/Panels/Page/Form/checkbox.react.js
+++ src/Components/Panels/Page/Form/checkbox.react.js
@@ -18,13 +18,20 @@
             var attributes = this.props.item.getAttributes();
             attributes.checked = this.state.checked;
             attributes.onChange = this.handleChange;
-
+            if (this.props.item.isChecked() != null){
             return (
                 <div>
                     <Label item={this.props.item} name={this.props.item.getLabel()} />
                 {React.DOM.input(attributes)}
                 </div>
             );
-        }
+            } else if (attributes.fallback == true){
+                <div>
+                    <Label item={this.props.item} name={this.props.item.getLabel()} />
+                <input type="hidden" name="some_flag" value="0" />
+                <input type="checkbox" name="some_flag" value="1" />
+                </div>
+            }
+            }        
     });
 });
