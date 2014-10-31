8f354df9ebb79d22d8e29a427c7706d5dcfa5bbc
Joris Blaak <joris@label305.com>
2014-10-31 11:21:57 +0100

    Working updating of tree

--- src/Components/Panels/Menu/sortable.react.js
+++ src/Components/Panels/Menu/sortable.react.js
@@ -27,22 +27,24 @@ define(['jstree'], function() {
 
         /**
          * Get all parents of an item
+         * @param items
          * @param item
          */
-        getParents: function(item) {
-            return this.props.item.getItems().filter(function(parent) {
+        getParents: function(items, item) {
+            return items.filter(function(parent) {
                 return this.isParentOf(parent, item);
             }.bind(this));
         },
 
         /**
          * Check if the parent is a direct parent of the child
+         * @param items
          * @param parent
          * @param child
          * @return boolean
          */
-        isDirectParentOf: function(parent, child) {
-            var allParentsOfChild = this.getParents(child);
+        isDirectParentOf: function(items, parent, child) {
+            var allParentsOfChild = this.getParents(items, child);
                         
             //Check if one of the parents is a child of the to be checked parent
             for(var i in allParentsOfChild) {
@@ -55,40 +57,43 @@ define(['jstree'], function() {
         
         /**
          * Check if the item has a parent
+         * @param items
          * @param check
          */
-        hasParent: function(check) {
-            return 0 != this.props.item.getItems().count(function(item) {
+        hasParent: function(items, check) {
+            return 0 != items.count(function(item) {
                     return this.isParentOf(item, check);
                 }.bind(this));
         },
 
         /**
          * Get direct children of an item
+         * @param items
          * @param parent
          * @return Array
          */
-        getDirectChildren: function(parent) {
-            return this.props.item.getItems().filter(function(child) {
-                return this.isChildOf(child, parent) && this.isDirectParentOf(parent, child);
+        getDirectChildren: function(items, parent) {
+            return items.filter(function(child) {
+                return this.isChildOf(child, parent) && this.isDirectParentOf(items, parent, child);
             }.bind(this));
         },
 
         /**
          * Add children recursively
+         * @param all
          * @param items
          * @return Array
          */
-        addChildren: function(items) {
+        addChildren: function(all, items) {
             return items.map(function(item) {
-                item.children = this.getDirectChildren(item);
-                item.children = this.addChildren(item.children);
+                item.children = this.getDirectChildren(all, item);
+                item.children = this.addChildren(all, item.children);
                 return item;
             }.bind(this));
         },
 
         /**
-         * Simplifies nested SortableItem objecrt to something much flatter
+         * Simplifies nested SortableItem object to something much flatter
          * @param tree
          * @param root
          */
@@ -100,7 +105,11 @@ define(['jstree'], function() {
                     type: root ? 'root' : 'child',
                     text: tree[i].getText(),
                     data: {
-                        id: tree[i].getId()
+                        id: tree[i].getId(),
+                        left: tree[i].getLeft(),
+                        right: tree[i].getRight(),
+                        text: tree[i].getText(),
+                        target: tree[i].getTarget()
                     },
                     state: {
                         opened: true
@@ -111,24 +120,30 @@ define(['jstree'], function() {
             
             return result;
         },
+
+        /**
+         * Get the simplified tree
+         */
+        simplifiedTree: function(items) {
+            //Fetch all items without any parent
+            var tree = items.filter(function(item) {
+                return !this.hasParent(items, item);
+            }.bind(this));
+
+            //Add children
+            tree = this.addChildren(items, tree);
+
+            //Simplify so that jsTree will like it
+            return this.simplifyForJsTree(tree, true);
+        },
         
         /**
          * Transform to the way JsTree want the tree
          */
-        getJsTree: function() {
-            //Fetch all items without any parent
-            var items = this.props.item.getItems();
-            var tree = items.filter(function(item) {
-                return !this.hasParent(item);
-            }.bind(this));
+        getJsTree: function(obj, cb) {
+            var tree = this.simplifiedTree(this.props.item.getItems());
             
-            //Add children
-            tree = this.addChildren(tree);
-            
-            //Simplify so that jsTree will like it
-            tree = this.simplifyForJsTree(tree, true);
-            
-            return tree;
+            cb.call(this, tree);
         },
 
         /**
@@ -154,6 +169,7 @@ define(['jstree'], function() {
                 //Add current item to the result
                 result.push({
                     id: tree[i].data.id,
+                    text: tree[i].data.text,
                     left: nextLeft,
                     right: nextRight                    
                 });
@@ -177,32 +193,81 @@ define(['jstree'], function() {
             console.log(tree);
             console.log(parse);
         },
+
+        /**
+         * Triggered when a node if focused
+         * @param id
+         */
+        onNodeFocus: function(id) {
+            var item = this.props.item.getItems().find(function(item) {
+                 return item.getId() == id;
+            });
+            
+            flux.actions.click(item.getTarget(), this.props.originPanel);
+        },
+
+        /**
+         * Listen for the tree to change
+         */
+        componentDidMount: function() {
+            $(document).on('dnd_stop.vakata', function (e, data) {
+                var tree = $(this.refs.tree.getDOMNode()).jstree(true).get_json('#', {});
+                this.treeChanged(tree);
+            }.bind(this));
+            
+            //Check if focused, if so dispatch click and try to unfocus
+            $(this.refs.tree.getDOMNode())
+                .on('select_node.jstree', function (e, data) {
+                    this.onNodeFocus(data.node.data.id);
+                    $(this.refs.tree.getDOMNode()).jstree('deselect_node', data.node);
+                }.bind(this))
+        },
+
+        /**
+         * Check if the component should update
+         * nextProps.item will always equal this.props.item since it is a pointer
+         * to an object, so we cannot compare the two
+         */
+        shouldComponentUpdate: function(nextProps, nextState) {
+            if(!this.initialized) {
+               return true;
+            }
+            
+            //Get what the tree looks like in the dom
+            var domTree = $(this.refs.tree.getDOMNode()).jstree(true).get_json('#', {});
+            domTree = this.parseTree(domTree, 1);
+            
+            //Check if the items in the next version correspond to items in the dom
+            var count = nextProps.item.getItems().count(function(item) {
+                return !domTree.none(function(domItem) {
+                    return item.getId() == domItem.id
+                            && item.getLeft() == domItem.left
+                            && item.getRight() == domItem.right
+                            && item.getText() == domItem.text
+                });
+            });
+            
+            return count != nextProps.item.getItems().length;
+        },
         
         componentDidUpdate: function() {
-            //Draw the tree if we're not initialized and we have received our items 
-            if(!this.initialized && this.props.item.getItems().length > 0) {
+            if(!this.initialized) {
                 this.initialized = true;
 
-                //Listen for some event
-                $(document).on('dnd_stop.vakata', function (e, data) {
-                    var tree = $(this.refs.tree.getDOMNode()).jstree(true).get_json('#', {});
-                    this.treeChanged(tree);
-                }.bind(this));
-                
                 $(this.refs.tree.getDOMNode())
-                    
+
                     //Initialize the tree
                     .jstree({
                         core: {
                             //Parse and set data
-                            data: this.getJsTree(),
-                            
+                            data: this.getJsTree,
+
                             //Make sure something updates when we drag
-                            check_callback : true,
-                            
+                            check_callback: true,
+
                             //More fancy more better
-                            animation : 1,
-                            
+                            animation: 1,
+
                             //No selection
                             multiple: false
                         },
@@ -215,8 +280,10 @@ define(['jstree'], function() {
                                 max_depth: 4
                             }
                         }
-                    });                
-            }  
+                    });
+            } else {
+                $(this.refs.tree.getDOMNode()).jstree('refresh');
+            }
         },
         render: function() {
             return (
