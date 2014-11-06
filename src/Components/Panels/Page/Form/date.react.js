8c9bd93848722f39c4f6ff6bbc76db08655bb6cd
Alexander van Brakel <alexander@label305.com>
2014-11-06 15:33:31 +0100

    Added datepicker

--- src/Components/Panels/Page/Form/date.react.js
+++ src/Components/Panels/Page/Form/date.react.js
@@ -7,36 +7,39 @@
  *
  * @jsx React.DOM
  */
-define(['build/Components/Panels/Page/Form/label.react', 'pikaday', 'pikadayjq'], function (Label) {
+ define(['build/Components/Panels/Page/Form/label.react', 'moment', 'pikaday', 'pikadayjq'], function (Label) {
     return React.createClass({
-        getInitialState: function () {
-            return {value: this.props.item.getValue()};
-        },
-        handleChange: function (event) {
-            this.setState({value: event.target.value});
-        },
-        render: function () {
+        componentDidMount: function() {
+         var $datepicker = $('#datepicker').pikaday({
+            firstDay: 1,
+            format: 'YYYY-MM-DD',
+        });
+     },
 
-           var $datepicker = $('#datepicker').pikaday({
-        firstDay: 1,
-        minDate: new Date('2000-01-01'),
-        maxDate: new Date('2020-12-31'),
-        yearRange: [2000,2020]
-    });
+
+     getInitialState: function () {
+        return {value: this.props.item.getValue()};
+    },
+    handleChange: function (event) {
+        this.setState({value: event.target.value});
+    },
+    render: function () {
+
+
     // chain a few methods for the first datepicker, jQuery style!
-    $datepicker.pikaday('show').pikaday('nextMonth');
-    
-            var attributes = this.props.item.getAttributes();
-            attributes.value = this.state.value;
-            attributes.onChange = this.handleChange;
-            attributes.id = 'datepicker';
 
-            return (
-                <div>
-                    <Label item={this.props.item} name={this.props.item.getLabel()} />
-                {React.DOM.input(attributes)}
-                </div>
-            );
-        }
-    });
+
+    var attributes = this.props.item.getAttributes();
+    attributes.value = this.state.value;
+    attributes.onChange = this.handleChange;
+    attributes.id = 'datepicker';
+
+    return (
+        <div>
+        <Label item={this.props.item} name={this.props.item.getLabel()} />
+        {React.DOM.input(attributes)}
+        </div>
+        );
+}
+});
 });
