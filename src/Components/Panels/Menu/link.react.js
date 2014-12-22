82419d36c3e6bebba661ebb89da938b7f22f6fce
Alexander van Brakel <alexander@label305.com>
2014-12-22 09:43:03 +0100

    Added active icon getting-setting

--- src/Components/Panels/Menu/link.react.js
+++ src/Components/Panels/Menu/link.react.js
@@ -14,21 +14,23 @@ define(['react'], function(React) {
             flux.actions.click(this.props.item.getTarget(), this.props.panel, this.props.item);
         },
         render: function() {
-            
+
             var className = 'menu-item-link auja-border-secondary ';
-            
+
             //Create the icon class
-            var icon = "fallback";
-            if(this.props.item.getIcon()) {
-                icon = this.props.item.getIcon();    
-            }
+            var icon = this.props.item.getIcon();
+
             className += "icon " + icon;
-            
+
             //Check if we match the active item
             if(this.props.item.isActive()) {
+                if (this.props.item.getActiveIcon()){
+                    var activeIcon = this.props.item.getActiveIcon();
+                    className = className.replace(icon, activeIcon);
+                }
                 className += " auja-color-main active auja-active-border-main";
             }
-            
+
             return (
                 <li className={className} onClick={this.handleClick}>
                     <span>{this.props.item.getText()}</span>
