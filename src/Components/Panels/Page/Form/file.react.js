699e87c5e4dcafcdabf51ad14780fc1eecc532f8
Joris Blaak <joris@label305.com>
2015-01-25 20:55:25 +0100

    Removed name

--- src/Components/Panels/Page/Form/file.react.js
+++ src/Components/Panels/Page/Form/file.react.js
@@ -16,11 +16,20 @@ define([
         componentDidMount: function () {
             //Dynamically add the element since we don't want to have React to bind its events to it
             var node = $(this.refs.file.getDOMNode()).append(
-                $('<input type="file" />')
+                $('<input type="file" name="file" data-name="file" />')
             );
 
             node.fileupload({
-                url: this.props.item.getTarget()
+                url: this.props.item.getTarget(),
+
+                //Transfer the reference we got from the upload response
+                done: function (e, data) {
+                    var ref = '';
+                    for (var i in data.result.files) {
+                        ref = data.result.files[0].ref;
+                    }
+                    $(this.refs.hidden.getDOMNode()).val(ref);
+                }.bind(this)
             });
         },
         render: function () {
@@ -28,6 +37,7 @@ define([
                 <div>
                     <Label item={this.props.item} name={this.props.item.getLabel()} />
                     <span ref="file"></span>
+                    <input type="text" name={this.props.item.getName()} ref="hidden" />
                 </div>
             );
         }
