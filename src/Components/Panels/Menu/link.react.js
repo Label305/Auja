a7c916fd3d185b301901bda3c69ba0360ed10fe9
Joris Blaak <joris@label305.com>
2015-02-25 14:06:46 +0100

    Added possibility to open link in outside of Auja

--- src/Components/Panels/Menu/link.react.js
+++ src/Components/Panels/Menu/link.react.js
@@ -8,32 +8,40 @@
  * @jsx React.DOM
  */
 
-define(['react'], function(React) {
+define(['react'], function (React) {
     return React.createClass({
-        handleClick: function() {
-            flux.actions.click(this.props.item.getTarget(), this.props.panel, this.props.item);
+        handleClick: function () {
+            if (!this.props.item.isExternal()) {
+                flux.actions.click(this.props.item.getTarget(), this.props.panel, this.props.item);
+            }
         },
-        render: function() {
-            
+        render: function () {
+
             var className = 'menu-item-link auja-border-secondary ';
-            
+
             //Create the icon class
             var icon = "fallback";
-            if(this.props.item.getIcon()) {
-                icon = this.props.item.getIcon();    
+            if (this.props.item.getIcon()) {
+                icon = this.props.item.getIcon();
             }
             className += "icon " + icon;
-            
+
             //Check if we match the active item
-            if(this.props.item.isActive()) {
+            if (this.props.item.isActive()) {
                 className += " auja-color-main active auja-active-border-main";
             }
-            
+
+            var link = <span>{this.props.item.getText()}</span>;
+            if (this.props.item.isExternal()) {
+                link = <a target="_blank" href={this.props.item.getTarget()}>{this.props.item.getText()}</a>;
+                className += " external";
+            }
+
             return (
                 <li className={className} onClick={this.handleClick}>
-                    <span>{this.props.item.getText()}</span>
+                    {link}
                 </li>
-                );
+            );
         }
     });
 
